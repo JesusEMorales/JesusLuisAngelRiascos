a7caaef27c46451d588f44ca177b504f119e7b40
JesusEMorales <jesusculajay116@gmail.com>
2026-09-02 08:28:13 -0600

    Cambios CSS Completos

--- codigo.js
+++ codigo.js
@@ -1,19 +1,9 @@
-let username = [];
-let password = [];
-
-
-function mostrar(id) {
-    document.getElementById("registrarse").className = "oculto";
-    document.getElementById("ingresar").className = "oculto";
-    document.getElementById(id).className = "visible";
-}
+let usuarios = [];
+let passes = [];
 
 function guardar() {
-    nombre = document.getElementById("nombrehtml").value;
-    username.push(nombre);
-
-}
-function guardar2() {
-    contrasena = document.getElementById("contrasenahtml").value;
-    password.push(contrasena);
+    usuarioagregado = document.getElementById("userinicio").value;
+    passagregado = document.getElementById("passinicio").value;
+    usuarios.push(usuarioagregado)
+    passes.push(passagregado)
 }

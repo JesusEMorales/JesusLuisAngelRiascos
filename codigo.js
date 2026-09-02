11f69e08b14ad08e1c0a373efbcf4ae8f2d574c0
JesusEMorales <jesusculajay116@gmail.com>
2026-09-02 09:18:13 -0600

    Java cambios

--- codigo.js
+++ codigo.js
@@ -1,9 +1,39 @@
 let usuarios = [];
 let passes = [];
 
-function guardar() {
+function guardaruser(){
     usuarioagregado = document.getElementById("userinicio").value;
+}
+
+function guardarpass(){
     passagregado = document.getElementById("passinicio").value;
+}
+
+function guardar(){
+    guardaruser()
+    guardarpass()
     usuarios.push(usuarioagregado)
     passes.push(passagregado)
+    alert("Usuario registrado exitosamente.")
+}
+
+function verificar(){ 
+    userv = document.getElementById("usuario").value;
+    passv = document.getElementById("pass").value;
+    for(let i = 0; i<=usuarios.length; i++){
+        if (userv == usuarios(i)){
+            for(let j = 0; i<= passes.length; i++){
+                if (passv == passes(i)){
+                    alert ("Inicio de sesión válido.");
+                }
+                else{
+                    alert ("Usuario o contraseña inválidos.");
+                }
+            }
+        }
+        else{
+            alert ("Usuario o contraseña inválidos.");
+        }
+
+    }
 }
